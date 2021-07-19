7ba0308e88579c217f6c0a0e461c3de243702cfc
Elijah Soladoye <shodown96@gmail.com>
2021-07-19 13:11:46 +0100

    first-nextjs-app

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   res.status(200).json({ text: "Hello" });
 }
 
-export default function handler(req, res) {
-  const email = req.body.email;
-  // Then save email to your database, etc...
-}
+// export default function handler(req, res) {
+//   const email = req.body.email;
+//   // Then save email to your database, etc...
+// }
